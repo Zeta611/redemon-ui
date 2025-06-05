d9e160617a23fb5be9debcb1e44bb2b49a74767e
Jay Lee <jaeho.lee@snu.ac.kr>
2025-06-06 00:56:55 +0900

    :fire: Remove unused config

--- next.config.ts
+++ next.config.ts
@@ -1,26 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  turbopack: {
-    resolveAlias: {
-      fs: [],
-      tty: [],
-      constants: [],
-      child_process: [],
-    },
-  },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.alias = {
-        ...config.resolve.alias,
-        fs: false,
-        tty: false,
-        constants: false,
-        child_process: false,
-      };
-    }
-    return config;
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
