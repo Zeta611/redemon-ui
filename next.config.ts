23003840aeaa5e458c29e43e9e59dddeaf338b6e
Jay Lee <jaeho.lee@snu.ac.kr>
2025-06-07 18:06:44 +0900

    :wrench: Remove info/debug logs in prod

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,11 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  compiler: {
+    removeConsole: {
+      exclude: ["error", "warn"],
+    },
+  },
+};
 
 export default nextConfig;
