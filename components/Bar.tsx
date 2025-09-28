fbb5ce8af7ea3fd11427f2b9f321c4de2fa60f74
Jay Lee <jaeho.lee@snu.ac.kr>
2025-09-28 13:45:52 +0900

    :lipstick: Right align the GitHub link

--- components/Bar.tsx
+++ components/Bar.tsx
@@ -20,50 +20,52 @@ export default function Bar() {
   const { chooseSample } = useAppState();
   return (
     <div className="dots-dense dots">
-      <div className="to-accent flex items-center gap-7 bg-linear-to-l to-85% px-3 py-2">
-        <Link href="/" className="font-rounded text-2xl font-bold">
-          🧙‍♀️ ReDemon UI
-        </Link>
-        <Menubar>
-          <MenubarMenu>
-            <MenubarTrigger className="text-md font-medium">
-              File
-            </MenubarTrigger>
-            <MenubarContent>
-              <MenubarSub>
-                <MenubarSubTrigger>Examples</MenubarSubTrigger>
-                <MenubarSubContent>
-                  <MenubarItem onClick={() => chooseSample("Counter")}>
-                    Counter
-                  </MenubarItem>
-                  <MenubarItem onClick={() => chooseSample("Text setter")}>
-                    Text setter
-                  </MenubarItem>
-                  <MenubarItem onClick={() => chooseSample("Calculator")}>
-                    Calculator
-                  </MenubarItem>
-                  <MenubarItem onClick={() => chooseSample("Todo list")}>
-                    Todo list
-                  </MenubarItem>
-                </MenubarSubContent>
-              </MenubarSub>
-              <MenubarSeparator />
-              <MenubarItem>Save</MenubarItem>
-            </MenubarContent>
-          </MenubarMenu>
-          <MenubarMenu>
-            <MenubarTrigger className="text-md font-medium">
-              Settings
-            </MenubarTrigger>
-            <MenubarContent>
-              <DialogTrigger asChild>
-                <MenubarItem>API Key</MenubarItem>
-              </DialogTrigger>
-            </MenubarContent>
-          </MenubarMenu>
-        </Menubar>
+      <div className="to-accent flex items-center justify-between bg-linear-to-l to-85% px-3 py-2">
+        <div className="flex gap-7">
+          <Link href="/" className="font-rounded text-2xl font-bold">
+            🧙‍♀️ ReDemon UI
+          </Link>
+          <Menubar>
+            <MenubarMenu>
+              <MenubarTrigger className="text-md font-medium">
+                File
+              </MenubarTrigger>
+              <MenubarContent>
+                <MenubarSub>
+                  <MenubarSubTrigger>Examples</MenubarSubTrigger>
+                  <MenubarSubContent>
+                    <MenubarItem onClick={() => chooseSample("Counter")}>
+                      Counter
+                    </MenubarItem>
+                    <MenubarItem onClick={() => chooseSample("Text setter")}>
+                      Text setter
+                    </MenubarItem>
+                    <MenubarItem onClick={() => chooseSample("Calculator")}>
+                      Calculator
+                    </MenubarItem>
+                    <MenubarItem onClick={() => chooseSample("Todo list")}>
+                      Todo list
+                    </MenubarItem>
+                  </MenubarSubContent>
+                </MenubarSub>
+                <MenubarSeparator />
+                <MenubarItem>Save</MenubarItem>
+              </MenubarContent>
+            </MenubarMenu>
+            <MenubarMenu>
+              <MenubarTrigger className="text-md font-medium">
+                Settings
+              </MenubarTrigger>
+              <MenubarContent>
+                <DialogTrigger asChild>
+                  <MenubarItem>API Key</MenubarItem>
+                </DialogTrigger>
+              </MenubarContent>
+            </MenubarMenu>
+          </Menubar>
+        </div>
         <Link href="https://github.com/Zeta611/redemon-ui">
-          <GitHub className="size-5 rounded-full bg-white" />
+          <GitHub className="size-5.5 rounded-full bg-white" />
         </Link>
       </div>
     </div>
